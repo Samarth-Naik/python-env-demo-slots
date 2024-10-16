925703f70e5d2f8e21ae09a78f7ade687a7e6dfa
Samarth-Naik <naiksamarth124@gmail.con.com>
2024-10-16 10:14:34 +0530

    updated files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World 2.0!</h1>'
+    return '<h1>Hello, World 3.0!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
